847cfa4a01480b3e62491f5e9e5ccda89652a343
wuyl <843794313@qq.com>
2018-07-02 13:43:29 +0800

    改用countdownlatch

--- src/main/java/com/test/simulation/service/TaskService.java
+++ src/main/java/com/test/simulation/service/TaskService.java
@@ -4,12 +4,14 @@ import com.alibaba.fastjson.JSONObject;
 import com.test.simulation.dao.NumberDao;
 import com.test.simulation.dao.NumberFlowDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by Administrator on 2018/6/28.
@@ -30,21 +32,29 @@ public class TaskService {
     private LimitService limitService;
 
     @Async
-    public void myTask(List<JSONObject> objects) {
+    public void myTask(List<JSONObject> objects,CountDownLatch latch) {
         try {
             limitService.require();
+            System.out.println(new SimpleDateFormat("hh:mm:ss").format(new Date())+" "+
+                    Thread.currentThread().getName()+" 请求接口");
             apiService.getData(objects);
             for (JSONObject obj :objects) {
                 JSONObject j = new JSONObject(3);
                 j.put("number",obj.get("number"));
-                j.put("flow",1);
+                j.put("flow",2);
                 j.put("time",new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
                 numberFlowDao.add(j);
 
+                obj.put("status",666);
                 numberDao.update(obj);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }finally {
+            if (latch != null){
+                latch.countDown();
+            }
+            System.out.println( Thread.currentThread().getName()+" 完成任务。");
         }
 
     }
--- src/test/java/com/test/simulation/SimulationApplicationTests.java
+++ src/test/java/com/test/simulation/SimulationApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,25 +30,35 @@ public class SimulationApplicationTests {
 	@Autowired
 	private ThreadPoolTaskExecutor executor;
 
+	private CountDownLatch latch;
+
 	@Test
 	public void contextLoads() throws Exception {
+		Integer latchSize = 0;
 		limitService.start();
 		int index = 0;
 		int size = 100;
 		List<JSONObject> list = numberDao.getList();
+		latchSize = list.size() / size;
+		if(list.size() % size > 0){
+			latchSize++;
+		}
+		latch = new CountDownLatch(latchSize);
 		while (index < list.size()){
 			if(index + size > list.size()){
 				size = list.size() - index;
 			}
 			List<JSONObject> sl = list.subList(index,index + size);
-			task.myTask(sl);
+			task.myTask(sl,latch);
 			index += size;
 		}
-		while (executor.getThreadPoolExecutor().getQueue().size()>1){
-			System.out.println(System.currentTimeMillis()+" queueSize:"+executor.getThreadPoolExecutor().getQueue().size());
-			Thread.sleep(1000);
-		}
+//		while (executor.getThreadPoolExecutor().getQueue().size()>1){
+//			System.out.println(System.currentTimeMillis()+" queueSize:"+executor.getThreadPoolExecutor().getQueue().size());
+//			Thread.sleep(1000);
+//		}
+		latch.await();
 		limitService.shutDown();
+		Thread.sleep(4000);
 		executor.shutdown();
 		System.out.println("完成。");
 	}
